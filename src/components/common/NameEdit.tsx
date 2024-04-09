eb0a4765747acc5041e023bb1616653eb858684a
tigerding <zhiyuanding01@gmail.com>
2024-04-09 10:56:45 -0400

    feat: adds confirmation btn for name edit and percent edit

--- src/components/common/NameEdit.tsx
+++ src/components/common/NameEdit.tsx
@@ -1,6 +1,6 @@
 import { SyntheticEvent, useState, useRef } from "react";
-import { Popover, Input, Button, ButtonProps } from "antd";
-import { EditOutlined } from "@ant-design/icons";
+import { Flex, Popover, Input, Button, ButtonProps } from "antd";
+import { EditOutlined, CheckOutlined } from "@ant-design/icons";
 
 interface NameEditProps {
   size: ButtonProps['size'],
@@ -10,6 +10,7 @@ interface NameEditProps {
 
 function NameEdit({ size, defaultValue, onChange }: NameEditProps) {
   const [open, setOpen] = useState(false);
+  const [edited, setEdited] = useState(false);
   const inputRef = useRef(null);
 
   const emitEvent = (event: SyntheticEvent) => {
@@ -23,13 +24,17 @@ function NameEdit({ size, defaultValue, onChange }: NameEditProps) {
   return (
     <Popover
       content={
-        <Input
-          ref={inputRef}
-          allowClear
-          defaultValue={defaultValue}
-          style={{ width: 120 }}
-          onPressEnter={emitEvent}
-        />
+        <Flex align="center">
+          <Input
+            ref={inputRef}
+            allowClear
+            defaultValue={defaultValue}
+            style={{ width: 120 }}
+            onChange={() => setEdited(true)}
+            onPressEnter={emitEvent}
+          />
+          {edited && <Button type="link" icon={<CheckOutlined />} onClick={emitEvent} />}
+        </Flex>
       }
       open={open}
       onOpenChange={(newOpen) => setOpen(newOpen)}
--- src/components/common/PercentEdit.tsx
+++ src/components/common/PercentEdit.tsx
@@ -1,6 +1,6 @@
 import { SyntheticEvent, useState } from "react";
 import { Button, Flex, Popover, Slider, InputNumber } from "antd";
-import { EditOutlined, CheckOutlined } from "@ant-design/icons";
+import { EditOutlined, CheckOutlined, CheckCircleOutlined } from "@ant-design/icons";
 
 interface PercentEditProps {
   defaultValue: number;
@@ -9,7 +9,13 @@ interface PercentEditProps {
 
 function PercentEdit({ defaultValue, onChange }: PercentEditProps) {
   const [open, setOpen] = useState(false);
+  const [edited, setEdited] = useState(false);
   const [value, setValue] = useState(defaultValue);
+
+  const changeValue = (value: number) => {
+    setValue(value);
+    setEdited(true);
+  };
 
   const emitEvent = (event: SyntheticEvent, value: number) => {
     setOpen(false);
@@ -24,21 +30,22 @@ function PercentEdit({ defaultValue, onChange }: PercentEditProps) {
       placement="bottom"
       content={
         <Flex align="center">
+          <Button type="link" icon={<CheckCircleOutlined />} onClick={(event) => emitEvent(event, 100)} />
           <Slider
             min={0}
             max={100}
             value={value}
             style={{ width: 100, marginRight: 10 }}
-            onChange={setValue} />
+            onChange={changeValue} />
           <InputNumber
             min={0}
             max={100}
             value={value}
             style={{ width: 65 }}
-            onChange={(value) => setValue(value ?? 0)}
+            onChange={(value) => changeValue(value ?? 0)}
             onPressEnter={(event) => emitEvent(event, value)}
           />
-          <Button type="link" icon={<CheckOutlined />} onClick={(event) => emitEvent(event, 100)} />
+          {edited && <Button type="link" icon={<CheckOutlined />} onClick={(event) => emitEvent(event, value)} />}
         </Flex>
       }
       open={open}
