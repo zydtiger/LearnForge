c8111bc885225659394e5de91dab38a397f18798
tigerding <zhiyuanding01@gmail.com>
2024-04-20 21:56:35 -0400

    feat: use esc to quit note and return to previous view

--- src/components/SkillNote.tsx
+++ src/components/SkillNote.tsx
@@ -1,14 +1,31 @@
+import { KeyboardEventHandler } from 'react';
 import { Typography } from "antd";
 import MDEditor from '@uiw/react-md-editor';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
-import { selectNoteViewNode, updateMarkdownNote } from '../redux/slices/viewSlice';
+import {
+  selectNoteViewNode,
+  selectPrevViewBeforeNote,
+  setViewMode,
+  updateMarkdownNote
+} from '../redux/slices/viewSlice';
 
 function SkillNote() {
   const nodeDatum = useAppSelector(selectNoteViewNode);
+  const prevView = useAppSelector(selectPrevViewBeforeNote);
   const dispatch = useAppDispatch();
 
+  const handleKeyDown: KeyboardEventHandler = (event) => {
+    if (event.key == 'Escape') {
+      dispatch(setViewMode(prevView)); // quits note view
+    }
+  };
+
   return (
-    <div data-color-mode="light" style={{ padding: 30 }}>
+    <div
+      onKeyDown={handleKeyDown}
+      data-color-mode="light"
+      style={{ padding: 30 }}
+    >
       <Typography.Title level={2} editable={true} style={{ top: 0, left: 0 }}>
         {nodeDatum.name}
       </Typography.Title>
--- src/redux/slices/viewSlice.ts
+++ src/redux/slices/viewSlice.ts
@@ -3,23 +3,30 @@ import { RootState } from "../store";
 import { RawNodeDatum } from 'react-d3-tree';
 import { DefaultRootNode } from '../../types/defaults';
 
+type ViewMode = 'tree' | 'list' | 'note';
+
 interface ViewState {
-  viewMode: 'tree' | 'list' | 'note'; // determines the current view
+  viewMode: ViewMode;                 // determines the current view
   isManualModalOpen: boolean;         // whether manual modal is open 
   noteViewNode: RawNodeDatum;         // the current node to edit in note view
+  prevViewBeforeNote: ViewMode;       // the previous view mode before note view
 }
 
 const initialState: ViewState = {
   viewMode: 'tree',
   isManualModalOpen: false,
   noteViewNode: DefaultRootNode,
+  prevViewBeforeNote: 'tree'
 };
 
 const viewSlice = createSlice({
   name: 'view',
   initialState,
   reducers: {
-    setViewMode(state, action: PayloadAction<typeof state.viewMode>) {
+    setViewMode(state, action: PayloadAction<ViewMode>) {
+      if (action.payload == 'note') {
+        state.prevViewBeforeNote = state.viewMode;
+      }
       state.viewMode = action.payload;
     },
     setIsManualModalOpen(state, action: PayloadAction<boolean>) {
@@ -39,5 +46,6 @@ export const { setViewMode, setIsManualModalOpen, setNoteViewNode, updateMarkdow
 export const selectViewMode = (state: RootState) => state.view.viewMode;
 export const selectIsManualModalOpen = (state: RootState) => state.view.isManualModalOpen;
 export const selectNoteViewNode = (state: RootState) => state.view.noteViewNode;
+export const selectPrevViewBeforeNote = (state: RootState) => state.view.prevViewBeforeNote;
 
 export default viewSlice.reducer;
