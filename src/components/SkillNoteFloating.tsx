14382d39dba5bcc873cc6ed1660184180fce0766
tigerding <zhiyuanding01@gmail.com>
2024-06-25 15:01:09 -0500

    feat: hide overflow and display ellipsis

--- src/components/SkillNoteFloating.tsx
+++ src/components/SkillNoteFloating.tsx
@@ -5,28 +5,64 @@ import {
   selectMouseCoords,
   selectNoteViewNode,
 } from "../redux/slices/noteSlice";
+import { useRef } from "react";
 
 function SkillNoteFloating() {
   const nodeDatum = useAppSelector(selectNoteViewNode);
   const isShow = useAppSelector(selectIsHovered);
   const mouseCoords = useAppSelector(selectMouseCoords);
 
+  const mdRef = useRef(null);
+  const isShowMore = (() => {
+    if (mdRef.current) {
+      const mdDisplay = mdRef.current as HTMLElement;
+      return mdDisplay.scrollHeight > 170;
+    }
+    return false;
+  })();
+
   return (
-    <div
-      hidden={!isShow}
-      style={{
-        position: "absolute",
-        left: `${mouseCoords[0]}px`,
-        top: `${mouseCoords[1]}px`,
-        width: 300,
-        height: 200,
-        background: "rgba(255,255,255,0.5)",
-        border: "1px solid black",
-        padding: 10,
-      }}
-    >
-      <Markdown>{nodeDatum.mdNote}</Markdown>
-    </div>
+    isShow && (
+      <div
+        style={{
+          position: "absolute",
+          left: `${mouseCoords[0]}px`,
+          top: `${mouseCoords[1]}px`,
+          width: 300,
+          height: 200,
+          boxSizing: "border-box",
+          background: "rgba(255,255,255,0.5)",
+          border: "1px solid black",
+          padding: 10,
+        }}
+      >
+        {/* Markdown display */}
+        <div
+          ref={mdRef}
+          style={{
+            overflow: "hidden",
+            height: 170,
+          }}
+        >
+          <Markdown>{nodeDatum.mdNote}</Markdown>
+        </div>
+
+        {/* Ellipsis for overflow */}
+        {isShowMore && (
+          <div
+            style={{
+              position: "absolute",
+              bottom: 10,
+              left: 10,
+              fontSize: 30,
+              color: "#1677FF",
+            }}
+          >
+            ...
+          </div>
+        )}
+      </div>
+    )
   );
 }
 
