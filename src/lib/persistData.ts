81dbaded304f0adfefe49c0eeca6322e01935669
tigerding <zhiyuanding01@gmail.com>
2024-06-25 11:16:09 -0500

    feat: browser import

--- src/lib/persistData.ts
+++ src/lib/persistData.ts
@@ -35,40 +35,47 @@ function writeStorage({ state }: { state: SkillsetState }) {
   );
 }
 
+async function readFile(fileHandle: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      resolve(reader.result as string);
+    });
+    reader.addEventListener("error", () => {
+      reject(reader.error);
+    });
+    reader.readAsText(fileHandle);
+  });
+}
+
 export async function invoke(endpoint: string, args?: Object) {
+  // sends to tauri backend directly
   if (TAURI_ENV) {
     return await tauriInvoke(endpoint, args as tauri.InvokeArgs);
   }
   // browser environment
-  switch (endpoint) {
-    case getStorageReadEndpoint():
-      return readStorage();
-
-    case getStorageWriteEndpoint():
-      writeStorage(args as { state: SkillsetState });
-      break;
-
-    case getStorageImportEndpoint():
-      // TODO:
-      break;
-
-    case getStorageExportEndpoint():
-      const { filePath } = args as { filePath: FileSystemFileHandle };
-      const parts = filePath.name.split(".");
-      const extension = parts[parts.length - 1];
-      let contents: string | Uint8Array;
-      if (extension == "lf") {
-        contents = JSON.stringify(readStorage());
-      } else {
-        const { payload } = args as { payload: ArrayBuffer };
-        contents = new Uint8Array(payload);
-      }
-      const writableStream = await filePath.createWritable();
-      await writableStream.write(contents);
-      await writableStream.close();
-      break;
-
-    default:
-      break;
+  if (endpoint == getStorageReadEndpoint()) {
+    return readStorage();
+  } else if (endpoint == getStorageWriteEndpoint()) {
+    writeStorage(args as { state: SkillsetState });
+  } else if (endpoint == getStorageImportEndpoint()) {
+    const { filePath } = args as { filePath: FileSystemFileHandle[] };
+    const fileHandle = await filePath[0].getFile();
+    const contents = await readFile(fileHandle);
+    writeStorage({ state: JSON.parse(contents) });
+  } else if (endpoint == getStorageExportEndpoint()) {
+    const { filePath } = args as { filePath: FileSystemFileHandle };
+    const parts = filePath.name.split(".");
+    const extension = parts[parts.length - 1];
+    let contents: string | Uint8Array;
+    if (extension == "lf") {
+      contents = JSON.stringify(readStorage());
+    } else {
+      const { payload } = args as { payload: ArrayBuffer };
+      contents = new Uint8Array(payload);
+    }
+    const writableStream = await filePath.createWritable();
+    await writableStream.write(contents);
+    await writableStream.close();
   }
 }
