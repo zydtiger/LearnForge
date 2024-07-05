0d2cd9dce8e457c7e57c7a5d740832dc2b25d0e1
tigerding <zhiyuanding01@gmail.com>
2024-07-05 12:35:48 -0500

    fix: hard code manual modal for compatibility

--- src/components/ManualModal.tsx
+++ src/components/ManualModal.tsx
@@ -1,5 +1,4 @@
 import { Button, Modal, Tabs, Table } from "antd";
-import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
 import rehypeSanitize from "rehype-sanitize";
 import { actions, convertToPlatformShortcuts } from "../lib/menu";
@@ -35,12 +34,23 @@ function convertShortcutsToTableData() {
 }
 
 function ManualModal({ isModalOpen, closeModal }: ManualModalProps) {
-  const [intro, setIntro] = useState("");
-  useEffect(() => {
-    fetch("/Manual.md").then((res) => {
-      res.text().then(setIntro);
-    });
-  });
+  const intro = `
+# LearnForge - Learning Goal Management and Logging App
+
+LearnForge is a comprehensive app that empowers you to manage and track your learning goals with ease. This helper provides an overview of LearnForge and its features.
+
+## Features
+
+- **Skill Tree Visualizer**: LearnForge features a captivating "skill tree" interface, reminiscent of tech trees in games like Stellaris. This visual representation allows you to organize and track your acquired skills under different categories, such as software, hardware, machine learning, and more.
+
+- **Goal Management**: With LearnForge, you can define your goals for skill acquisition. The app enables you to track progress and mark completed goals, facilitating efficient goal management.
+
+- **Logging Progress**: Keep a record of your learning journey by logging your progress. LearnForge allows you to add notes, milestones, and achievements in markdown format, providing a comprehensive overview of your process.
+
+- **Category-based Organization**: Categorize your skills based on different domains, such as programming languages, frameworks, or specific areas of expertise. This categorization simplifies navigation and enables you to focus on specific skill sets.
+
+- **Cross-Platform Compatibility**: LearnForge is built using Tauri, React, and TypeScript (ts). This technology stack ensures cross-platform compatibility, allowing you to run the app locally on various operating systems.
+  `;
 
   // define tab items
   const items = [
