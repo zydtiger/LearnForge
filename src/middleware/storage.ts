4fb9e6f2d2521230a205a1480ff4d99fc07da7dd
tigerding <zhiyuanding01@gmail.com>
2024-07-25 12:27:38 -0500

    fix: silent tauri warning in non-tauri environment by dynamically importing

--- src/middleware/storage.ts
+++ src/middleware/storage.ts
@@ -4,7 +4,6 @@ import {
   getStorageImportEndpoint,
   getStorageExportEndpoint,
 } from "../constants/endpoints";
-import { invoke as tauriInvoke, tauri } from "@tauri-apps/api";
 import { DefaultRootNode } from "../types/defaults";
 import { SkillsetState } from "../redux/slices/skillsetSlice";
 import { TAURI_ENV } from "../constants/env";
@@ -45,10 +44,11 @@ async function readFile(fileHandle: File): Promise<string> {
   });
 }
 
-export async function invoke(endpoint: string, args?: Object) {
+export async function invoke(endpoint: string, args?: Record<string, unknown>) {
   // sends to tauri backend directly
   if (TAURI_ENV) {
-    return await tauriInvoke(endpoint, args as tauri.InvokeArgs);
+    const api = await import("@tauri-apps/api");
+    return await api.invoke(endpoint, args);
   }
   // browser environment
   if (endpoint == getStorageReadEndpoint()) {
